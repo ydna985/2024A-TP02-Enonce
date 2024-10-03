c81f93fde28c1e1fd62f34d1493a812652eda1bf
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2024-10-03 11:22:39 -0400

    changed value names

--- TP2.py
+++ TP2.py
@@ -1,11 +1,12 @@
 """
 TP2 : Système de gestion de livres pour une bibliothèque
 
-Groupe de laboratoire : XX
-Numéro d'équipe :  YY
-Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
+Groupe de laboratoire : 06
+Numéro d'équipe :  12
+Noms et matricules : Koutou Andy Abdoul Mouhaimine (2390725), Nom2 (Matricule2)
 """
 import csv
+import datetime
 ########################################################################################################## 
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
 ########################################################################################################## 
@@ -15,12 +16,11 @@ csvfile=open("collection_bibliotheque.csv", "r")
 data=csv.reader(csvfile)
 library={}
 for line in data:
-    library[line[3]]={"title":line[0], "author":line[1], "publication date":line[2]}
+    library[line[3]]={"titre":line[0], "auteur":line[1], "date_publication":line[2]}
 csvfile.close()
 for key in library:
     print(key, library[key])
 print("\n")
-#print(f' \n Bibliotheque initiale : {library} \n')
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
@@ -31,15 +31,15 @@ csvfile=open("nouvelle_collection.csv", "r")
 data=csv.reader(csvfile)
 new_collection={}
 for line in data:
-    new_collection[line[3]]={"title":line[0], "author":line[1], "publication date":line[2]}
+    new_collection[line[3]]={"titre":line[0], "auteur":line[1], "date_publication":line[2]}
 csvfile.close()
 
 for key,data in new_collection.items():
     if  key in library:
-        print(f"Le livre {key} ---- {data["title"]} par {data["author"]} ---- est déjà présent dans la bibliothèque")
+        print(f"Le livre {key} ---- {data["titre"]} par {data["auteur"]} ---- est déjà présent dans la bibliothèque")
     else:
         library[key]=data
-        print(f"Le livre {key} ---- {data["title"]} par {data["author"]} ---- a été ajouté avec succès")
+        print(f"Le livre {key} ---- {data["titre"]} par {data["auteur"]} ---- a été ajouté avec succès")
 print("\n")
 
 
@@ -53,7 +53,7 @@ print("\n")
 keys_to_delete={}
 keys_to_add={}
 for key,data in library.items():
-    if data["author"] == "William Shakespeare":
+    if data["auteur"] == "William Shakespeare":
         print(key, data)
         new_key="WS"+key[1:4]
         keys_to_add[new_key]=data
@@ -64,10 +64,8 @@ for key,data in keys_to_add.items():
     library[key]=data
 
 for key in library:
-    if "WS" in key:
-        print(key, library[key])
+    print(key, library[key])      
 print("\n")
-#print(f' \n Bibliotheque avec modifications de cote : {library} \n')
 
         
 
@@ -89,11 +87,19 @@ print("\n")
 
 
 
+
+
+
 ########################################################################################################## 
 # PARTIE 5 : Livres en retard 
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+
+
+
+
+
 
 
 
